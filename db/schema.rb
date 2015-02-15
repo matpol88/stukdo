019f4cf18f66eb1bf84a9ce966fd621110b52188
Matthew <matpol88@gmail.com>
2015-02-15 00:19:22 -0500

    added scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150215042417) do
+ActiveRecord::Schema.define(version: 20150215051327) do
+
+  create_table "tasks", force: true do |t|
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
